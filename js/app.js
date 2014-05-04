5a3fc0530a24f45f7ce56e95b15cbc981c897b73
Xennis <xennis@online.de>
2014-05-04 13:23:47 +0200

    Add OpenStreetMap

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
+var mapDisplay = new MapDisplay('map');
+var map = mapDisplay.map;
+var mapFirstTimeResize = true;
+
 $(document).on("pageinit", function () {
     
     $.mobile.defaultPageTransition = 'slidefade';
     
     $( "body>[data-role='panel']" ).panel();
-    
     $("[data-role='navbar']").navbar();
     $("[data-role='header'], [data-role='footer']").toolbar();
-    
-    $("[data-role='listview']").listview().listview('refresh');    
+    $("[data-role='listview']").listview().listview('refresh');  
 });
+/*
 // Update the contents of the toolbars
 $(document).on("pageshow", "[data-role='page']", function () {
+	
     // Each of the four pages in this demo has a data-title attribute
     // which value is equal to the text of the nav button
     // For example, on first page: <div data-role="page" data-title="Info">
@@ -22,7 +26,15 @@ $(document).on("pageshow", "[data-role='page']", function () {
     // Add active class to current nav button
     $("[data-role='navbar'] a").each(function () {
         if ($(this).text() === current) {
-//            $(this).addClass("ui-btn-active");
+            $(this).addClass("ui-btn-active");
         }
-    });
+    });	
+});
+*/
+
+$("#mapView").on("pageshow", function() {
+	if (mapFirstTimeResize) {
+		mapFirstTimeResize = false;
+		map._onResize(); 
+	}
 });
